6b43df7245dfc6648478c7f6865bd02664b389bc
oyama919 <oyama919@gmail.com>
2018-01-10 00:12:27 +0900

    add blogs/new create new blog

--- express_api/app.js
+++ express_api/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('apidata.sqlite3');
 
@@ -8,6 +9,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/api/blogs', function(req, res) {
   db.serialize(() => {
@@ -18,6 +21,14 @@ app.get('/api/blogs', function(req, res) {
     });
   });
 });
+
+/* Post new */
+app.post('/api/blogs/new', function(req, res, next) {
+  var tle = req.body.title;
+  console.log(tle);
+  db.run('insert into blogs (title) values (?)',[tle]);
+});
+
 
 //start: node-dev app.js (npm i -g node-dev)
 var server = app.listen(3001, function() {
--- src/blogs/New.js
+++ src/blogs/New.js
@@ -1,29 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class NewBlog extends Component {
 
-    constructor(props){
-      super(props);
-      this.state = {title: ''};
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    };
+  }
 
-    onChangeValue(e) {
-      this.setState({[e.target.name] : e.target.value});
-    }
+  onChangeValue(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-    handleSubmit(e) {
-      e.preventDefault();
-      console.log(e.target.title.value);
-    }
+  handleSubmit(e) {
+    e.preventDefault();
+    const HEADERS = new Headers();
+    HEADERS.append('Content-Type', 'application/json');
+    const title = e.target.title.value;
+    let post_data = {title};
+    let options = {
+      method: 'POST',
+      headers: HEADERS,
+      mode: 'cors',
+      cache: 'default',
+      body: JSON.stringify(post_data)
+    };
 
-    render(){
-      return (
-        <form onSubmit={ this.handleSubmit }>
-          <input type="text" name="title" onChange={(e) => this.onChangeValue(e)} value={this.state.title} />
-          <input type="submit" value="送信" />
-        </form>
-      )
-    }
+    var request = new Request('http://localhost:3001/api/blogs/new', options);
+
+    fetch(request).then(function(response) {
+      console.log("Success");
+      return response;
+    })
+    .then(function(json) {
+      console.log(json.errors);
+    })
+    .catch(function(err) {
+      console.log("Error " + err);
+    })
+
+  };
+
+  render() {
+    return (<form onSubmit={this.handleSubmit}>
+      <input type="text" name="title" onChange={(e) => this.onChangeValue(e)} value={this.state.title}/>
+      <input type="submit" value="送信"/>
+    </form>)
+  }
 }
 
 export default NewBlog;
